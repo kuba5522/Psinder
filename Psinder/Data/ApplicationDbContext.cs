1f37eca6a530f04f52d563a0b7658413f7ad5583
Jakub Olejnik <jakub.p.olejnik@gmail.com>
2023-02-21 20:18:59 +0100

    Add project files.

---
+++ Psinder/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Psinder.Models;
+
+namespace Psinder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Post>? Posts { get; set; }
+        //do 
+        //add-migration PostsMigration
+        //update-database
+
+    }
+}
---
+++ Psinder/Data/Migrations/20230221184715_PostsMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Psinder.Data.Migrations
+{
+    public partial class PostsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Posts");
+        }
+    }
+}
