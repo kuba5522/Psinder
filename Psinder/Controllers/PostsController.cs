3b3bcdccf38f6176b7e6935ae2977bb7b6de4e73
Kuba <jakub.p.olejnik@gmail.com>
2023-03-04 12:20:40 +0100

    added create posts handle

--- Psinder/Controllers/PostsController.cs
+++ Psinder/Controllers/PostsController.cs
@@ -54,7 +54,7 @@ namespace Psinder.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description")] Post post)
+        public async Task<IActionResult> Create([Bind("Id,Title,Name, Age, Size, Breed, Difficulty,Location,Description, ImagePath, ContactPhone, ContactEmail")] Post post)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Psinder.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Name, Age, Size, Breed, Difficulty,Location,Description, ImagePath, ContactPhone, ContactEmail")] Post post)
         {
             if (id != post.Id)
             {
