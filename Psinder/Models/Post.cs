ece20e034ca9b7594b7b6940a132f2241c419d1d
pawel.michalak@primesoft.pl <pawel.michalak@primesoft.pl>
2023-03-05 10:02:46 +0100

    Dodaje displayname w klasie POST

--- Psinder/Models/Post.cs
+++ Psinder/Models/Post.cs
@@ -8,19 +8,29 @@ namespace Psinder.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name="Tytuł ogłoszenia")]
         public string Title { get; set; }
+        [Display(Name = "Imię")]
         public string Name { get; set; }
+        [Display(Name = "WIek")]
         public int Age { get; set; }
+        [Display(Name = "Rozmiar")]
         public string Size { get; set; }
+        [Display(Name = "Rasa")]
         public string Breed { get; set; }
+        [Display(Name = "Trudność")]
         public string Difficulty { get; set; }
+        [Display(Name = "Lokalizacja")]
         public string Location { get; set; }
 
+        [Display(Name = "Opis")]
         public string Description { get; set; }
 
         public string? ImagePath { get; set; }
 
+        [Display(Name = "Telefon kontaktowy")]
         public string ContactPhone { get; set; }
+        [Display(Name = "Email kontaktowy")]
         public string ContactEmail { get; set; }
     }
 }
